f8ce7ff9457a8a5c9ac62091765b95559c9e72c1
Maduranga Kodithuwakku <madurangakodithuwakku@gmail.com>
2022-07-15 16:03:20 +0530

    hobbies section added

---
+++ frontend_react/src/container/Hobbies/Hobbies.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { AppWrap, MotionWrap } from "../../wrapper";
+import ReactPlayer from "react-player/youtube";
+import "./Hobbies.scss";
+import { images } from "../../constants";
+
+const Hobbies = () => {
+  return (
+    <>
+      <h2 className="head-text">
+        My <span>Hobbies</span>
+      </h2>
+
+      <div className="app__hobbies-container">
+        <motion.div
+          className="app__hobbies-video"
+          whileInView={{ opacity: 1 }}
+          whileHover={{ scale: 1.1 }}
+          transition={{ duration: 0.5, type: "tween" }}
+        >
+          <ReactPlayer
+            url="https://www.youtube.com/watch?v=WhVArsoNnO8"
+            controls={true}
+            width="100%"
+            height="18rem"
+            loop={true}
+          />
+        </motion.div>
+        <motion.div
+          className="app__hobbies-des"
+          whileInView={{ opacity: 1 }}
+          whileHover={{ scale: 1.1 }}
+          transition={{ duration: 0.5, type: "tween" }}
+        >
+          <h2 className="bold-text" style={{ marginTop: 20 }}>
+            Drumming & Percussion
+          </h2>
+          <p className="p-text" style={{ marginTop: 10 }}>
+            Music gives a soul to the universe, wings to the mind, flight to the
+            imagination, and life to everything.” Therefore to relax my mind and
+            for new thoughts, I play the percussion instruments.
+          </p>
+          <button
+            type="button"
+            className="p-text"
+            onClick={() =>
+              window.open(
+                "https://www.youtube.com/c/MadurangaKodithuwakku/videos",
+                "_blank"
+              )
+            }
+          >
+            See My Other Videos
+          </button>
+        </motion.div>
+      </div>
+      <div className="app__hobbies-container">
+        <motion.div
+          className="app__hobbies-video"
+          whileInView={{ opacity: 1 }}
+          whileHover={{ scale: 1.1 }}
+          transition={{ duration: 0.5, type: "tween" }}
+        >
+          <img src={images.photos} alt="photos" />
+        </motion.div>
+        <motion.div
+          className="app__hobbies-des"
+          whileInView={{ opacity: 1 }}
+          whileHover={{ scale: 1.1 }}
+          transition={{ duration: 0.5, type: "tween" }}
+        >
+          <h2 className="bold-text" style={{ marginTop: 20 }}>
+            Photography
+          </h2>
+          <p className="p-text" style={{ marginTop: 10 }}>
+            "Photography is the story I fail to put into words.” Therefore I
+            like to capture the beauty and the moments in nature.
+          </p>
+          <button
+            type="button"
+            className="p-text"
+            onClick={() =>
+              window.open("https://www.facebook.com/PhotographyRanga", "_blank")
+            }
+          >
+            See My Page
+          </button>
+        </motion.div>
+      </div>
+    </>
+  );
+};
+
+export default AppWrap(
+  MotionWrap(Hobbies, "app__hobbies"),
+  "hobbies",
+  "app__primarybg"
+);
