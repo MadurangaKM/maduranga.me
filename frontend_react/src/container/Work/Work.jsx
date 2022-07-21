b5438599796bf687744e7347ca39fdec1d8b995c
Maduranga Kodithuwakku <madurangakodithuwakku@gmail.com>
2022-07-21 08:58:22 +0530

    portfolio hover issue fixed

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -42,17 +42,16 @@ const Work = () => {
                   staggerChildren: 0.5,
                 }}
                 className="app__work-hover app__flex"
+                onClick={() => window.open(work.projectLink, "_blank")}
               >
-                <a href={work.projectLink} target="_blank" rel="noreferrer">
-                  <motion.div
-                    whileInView={{ scale: [0, 1] }}
-                    whileHover={{ scale: [1, 0.9] }}
-                    transition={{ duration: 0.25 }}
-                    className="app__flex"
-                  >
-                    <AiFillEye />
-                  </motion.div>
-                </a>
+                <motion.div
+                  whileInView={{ scale: [0, 1] }}
+                  whileHover={{ scale: [1, 0.9] }}
+                  transition={{ duration: 0.25 }}
+                  className="app__flex"
+                >
+                  <AiFillEye />
+                </motion.div>
               </motion.div>
             </div>
 
