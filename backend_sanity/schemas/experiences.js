932e6b2343f1c9ae43d9879272916c4b26d273d1
MadurangaKM <madurangakodithuwakku@gmail.com>
2024-12-26 06:32:50 +0530

    sorting number added

--- backend_sanity/schemas/experiences.js
+++ backend_sanity/schemas/experiences.js
@@ -14,5 +14,10 @@ export default{
             type:'array',
             of:[{ type:'workExperience'}]
         },
+        {
+            name:'number',
+            title:'Number',
+            type:'number'
+        }, 
     ]
 }
