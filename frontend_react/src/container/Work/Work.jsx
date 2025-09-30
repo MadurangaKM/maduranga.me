261e03aa676c4f141ec19a81bad20562e50da205
MadurangaKM <madurangakodithuwakku@gmail.com>
2025-09-30 09:15:45 +0530

    style issue fixed

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -49,7 +49,7 @@ const Work = (props) => {
           content="Hi! I'm Maduranga Kodithuwakku. A Lead UI/UX Engineer based in Colombo, Sri Lanka. 5+ years of UX Industrial Experience, Crafting Digital Products, Web/Mobile Applications. I have worked with a couple of Enterprises, and UX Companies in Sri Lanka and also worldwide. I design for a user-centered focus. I'm a simple, yet efficient person, a hard worker, and really interested in art. Here is my dribble portfolio: https://dribbble.com/MadurangaDesign "
         />
       </Helmet>
-      <h2 className="head-text text_color">
+      <h2 className="head-text text_color" style={{ marginTop: "2rem" }}>
         My Creative <span>Portfolio</span>
       </h2>
 
